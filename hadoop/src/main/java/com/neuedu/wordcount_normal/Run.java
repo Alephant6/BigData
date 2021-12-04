7deebdf453f5d1ad514a7235ae5dac28599faa3d
Shengzhou Qiang <961774189@qq.com>
2021-12-04 11:35:29 +0800

    add MapReduce-wordcount
    介绍MapReduce分布式计算框架，yarn分布式资源管理系统；给出一个示例：词频统计的普通版本。

---
+++ hadoop/src/main/java/com/neuedu/wordcount_normal/Run.java
@@ -0,0 +1,91 @@
+package com.neuedu.wordcount_normal;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FSDataInputStream;
+import org.apache.hadoop.fs.FileStatus;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+
+/**
+ * 运行类：提交MapReduce任务，完成并行计算，完成词频统计任务
+ *
+ * @author Alephant
+ */
+public class Run {
+    public static void main(String[] args) {
+        // 构建配置对象
+        Configuration conf = new Configuration();
+        // 配置hadoop集群属性
+        conf.set("fs.defaultFS", "hdfs://master:9000");
+        try {
+            // 获取HDFS对象
+            FileSystem hdfs = FileSystem.get(conf);
+            // 定义输入目录
+            Path inputPath = new Path("/books");
+            // 定义输出目录
+            Path outputPath = new Path("/wordcount_result");
+            // 输出目录存在之则删除
+            if (hdfs.exists(outputPath)) {
+                hdfs.delete(outputPath, true);
+            }
+            // 创建任务
+            Job job = Job.getInstance(conf, "wordcount");
+            // 设置运行类
+            job.setJarByClass(Run.class);
+            // 设置输入
+            job.setInputFormatClass(TextInputFormat.class);
+            FileInputFormat.setInputPaths(job,inputPath);
+            // 设置Mapper
+            job.setMapperClass(WordCountMapper.class);
+            job.setMapOutputKeyClass(Text.class);
+            job.setMapOutputValueClass(IntWritable.class);
+            // 设置Reducer
+            job.setReducerClass(WordCountReducer.class);
+            job.setOutputKeyClass(Text.class);
+            job.setOutputValueClass(IntWritable.class);
+            // 设置输出
+            job.setOutputFormatClass(TextOutputFormat.class);
+            FileOutputFormat.setOutputPath(job, outputPath);
+            // 运行
+            boolean flag = job.waitForCompletion(true);
+            // 查看结果
+            System.out.println("词频统计结束\n单词\t次数");
+            for (FileStatus s : hdfs.listStatus(outputPath)){
+                // 目录包含子目录、文件
+                // FileStatus方法getXXX获取指定信息
+                // FileStatus方法isXXX判断
+                if (s.isFile()){
+                    // 打开文件
+                    FSDataInputStream reader = hdfs.open(s.getPath());
+                    // 构建缓冲读取器对象
+                    BufferedReader br = new BufferedReader(new InputStreamReader(reader));
+                    // 循环读取每一行文本
+                    String line = br.readLine();
+                    while (null != line) {
+                        // 输出读取的一行文本
+                        System.out.println(line);
+                        // 继续读取
+                        line = br.readLine();
+                    }
+                    // 关闭读取器
+                    br.close();
+                    // 关闭文件
+                    reader.close();
+                }
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+}
