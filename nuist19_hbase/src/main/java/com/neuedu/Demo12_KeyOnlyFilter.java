560ec814a26f45a330b3839f2b02bff8e62035bf
Shengzhou Qiang <961774189@qq.com>
2022-03-31 09:35:00 +0800

    过滤器

---
+++ nuist19_hbase/src/main/java/com/neuedu/Demo12_KeyOnlyFilter.java
@@ -0,0 +1,79 @@
+package com.neuedu;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hbase.Cell;
+import org.apache.hadoop.hbase.CellUtil;
+import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.TableName;
+import org.apache.hadoop.hbase.client.*;
+import org.apache.hadoop.hbase.filter.Filter;
+import org.apache.hadoop.hbase.filter.KeyOnlyFilter;
+import org.apache.hadoop.hbase.filter.PrefixFilter;
+import org.apache.hadoop.hbase.util.Bytes;
+
+import java.io.IOException;
+
+public class Demo12_KeyOnlyFilter {
+    public static void main(String[] args) {
+        // 创建Configuration对象
+        Configuration conf = HBaseConfiguration.create();
+        // 设置集群属性：若提供hbase-site.xml时，自动读取配置信息
+        // conf.set("hbase.zookeeper.property.clientPort","2181");
+        // conf.set("hbase.zookeeper.quorum","master,slave1,slave2");
+        try {
+            // 创建Connection对象
+            Connection conn = ConnectionFactory.createConnection(conf);
+            // 定义表名
+            String tableName = "employee";
+            TableName tn = TableName.valueOf(tableName);
+            // 获取表对象
+            Table table = conn.getTable(tn);
+            // 定义Scan全表扫描对象
+            Scan scan = new Scan();
+            // 定义过滤器对象
+//            Filter filter = new KeyOnlyFilter();
+            Filter filter = new KeyOnlyFilter(true);
+            // 设置过滤器
+            scan.setFilter(filter);
+            // 读取表中所有数据
+            ResultScanner rows = table.getScanner(scan);
+            // 打印表头
+            System.out.println("学号\t\t年龄\t\t姓名\t\t性别");
+            // 遍历所有行
+            for (Result r : rows) {
+                // 读取行键
+                String id = Bytes.toString(r.getRow());
+                System.out.print(id);
+                // 遍历所有单元格：列簇、列、值
+                for (Cell cell : r.rawCells()) {
+                    // 读取行键
+                    //String id = Bytes.toString(CellUtil.cloneRow(cell));
+                    // 读取列族
+                    //String f = Bytes.toString(CellUtil.cloneFamily(cell));
+                    //System.out.print("\t列簇：" + f);
+                    // 读取列名
+                    String c = Bytes.toString(CellUtil.cloneQualifier(cell));
+                    //System.out.print("\t列名：" + c);
+                    // 读取单元格的值：根据单元格的存储类型，进行特定的转换
+                    switch (c) {
+                        case "name":
+                        case "sex":
+                            String v1 = Bytes.toString(CellUtil.cloneValue(cell));
+                            System.out.print("\t\t" + v1);
+                            break;
+                        case "age":
+                            int v2 = Bytes.toInt(CellUtil.cloneValue(cell));
+                            System.out.print("\t" + v2);
+                            break;
+                    }
+                }
+                System.out.println();
+            }
+            table.close();
+            conn.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("失败~~~");
+        }
+    }
+}
---
+++ nuist19_hbase/src/main/java/com/neuedu/Demo16_QualifierFilter.java
@@ -0,0 +1,84 @@
+package com.neuedu;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hbase.*;
+import org.apache.hadoop.hbase.client.*;
+import org.apache.hadoop.hbase.filter.*;
+import org.apache.hadoop.hbase.util.Bytes;
+
+import java.io.IOException;
+
+public class Demo16_QualifierFilter {
+    public static void main(String[] args) {
+        // 创建Configuration对象
+        Configuration conf = HBaseConfiguration.create();
+        // 设置集群属性：若提供hbase-site.xml时，自动读取配置信息
+        // conf.set("hbase.zookeeper.property.clientPort","2181");
+        // conf.set("hbase.zookeeper.quorum","master,slave1,slave2");
+        try {
+            // 创建Connection对象
+            Connection conn = ConnectionFactory.createConnection(conf);
+            // 定义表名
+            String tableName = "employee";
+            TableName tn = TableName.valueOf(tableName);
+            // 获取表对象
+            Table table = conn.getTable(tn);
+            // 定义Scan全表扫描对象
+            Scan scan = new Scan();
+
+            // 定义列名
+            byte[] c1 = Bytes.toBytes("age");
+            byte[] c2 = Bytes.toBytes("ag");
+            // 定义特定的比较器
+            // ByteArrayComparable comparator = new BinaryComparator(c1);//比较
+            ByteArrayComparable comparator = new BinaryPrefixComparator(c2);//前缀比较
+            // ByteArrayComparable comparator = new SubstringComparator("ag");//字串比较
+            //ByteArrayComparable comparator = new RegexStringComparator("^age$");//正则表达式比较
+            // 定义过滤器对象
+            // 仅返回符合条件的行键，满足条件的列，并不是返回所有列
+            Filter filter = new QualifierFilter(CompareOperator.EQUAL, comparator);
+
+            // 设置过滤器
+            scan.setFilter(filter);
+            // 读取表中所有数据
+            ResultScanner rows = table.getScanner(scan);
+            // 打印表头
+            System.out.println("学号\t\t年龄\t\t姓名\t\t性别");
+            // 遍历所有行
+            for (Result r : rows) {
+                // 读取行键
+                String id = Bytes.toString(r.getRow());
+                System.out.print(id);
+                // 遍历所有单元格：列簇、列、值
+                for (Cell cell : r.rawCells()) {
+                    // 读取行键
+                    //String id = Bytes.toString(CellUtil.cloneRow(cell));
+                    // 读取列族
+                    //String f = Bytes.toString(CellUtil.cloneFamily(cell));
+                    //System.out.print("\t列簇：" + f);
+                    // 读取列名
+                    String c = Bytes.toString(CellUtil.cloneQualifier(cell));
+                    //System.out.print("\t列名：" + c);
+                    // 读取单元格的值：根据单元格的存储类型，进行特定的转换
+                    switch (c) {
+                        case "name":
+                        case "sex":
+                            String v1 = Bytes.toString(CellUtil.cloneValue(cell));
+                            System.out.print("\t\t" + v1);
+                            break;
+                        case "age":
+                            int v2 = Bytes.toInt(CellUtil.cloneValue(cell));
+                            System.out.print("\t" + v2);
+                            break;
+                    }
+                }
+                System.out.println();
+            }
+            table.close();
+            conn.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("失败~~~");
+        }
+    }
+}
